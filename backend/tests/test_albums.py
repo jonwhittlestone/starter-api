d0209502401176d3128ed937e38791bab5842b11
Jon Whittlestone <dev@howapped.com>
2023-06-21 06:16:23 +0100

    Test health

--- backend/tests/test_albums.py
+++ backend/tests/test_albums.py
@@ -5,21 +5,7 @@ from ..main import app
 client = TestClient(app)
 
 
-def test_root():
+def test_health():
     response = client.get("/health")
     print(response)
     assert response.status_code == 200
-
-# def test_create_album(test_app, monkeypatch):
-#     test_data = {"name": "something", 
-#                  "artist": "yazz", 
-#                  "year": 1981}
-
-#     def mock_post(db_session, payload):
-#         return test_data
-
-#     monkeypatch.setattr(crud, "post", mock_post)
-
-#     response = test_app.post("/notes/", content=json.dumps(test_data),)
-#     assert response.status_code == 201
-#     assert response.json() == test_data
