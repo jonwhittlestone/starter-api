5c5fbe965472f79f6d966ce6fe7da9bc8a75dd19
Jon Whittlestone <dev@howapped.com>
2023-07-21 06:46:11 +0100

    Simple create test with session_override and sqllite testing db

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,6 +1,24 @@
 import pytest
-from starlette.testclient import TestClient
 
-from ..main import app
+from fastapi.testclient import TestClient
+from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel.pool import StaticPool
 
 
+@pytest.fixture(name="session")
+def session_fixture():
+    # in memory db
+    engine = create_engine(
+        "sqlite://",  #
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,  #
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+
+@pytest.fixture(name="client")  #
+def client_fixture(session: Session):  #
+    def get_session_override():  #
+        return session
--- backend/tests/test_albums.py
+++ backend/tests/test_albums.py
@@ -1,4 +1,5 @@
 import pytest
+from fastapi.testclient import TestClient
 from httpx import AsyncClient
 
 from ..main import app
@@ -9,4 +10,14 @@ async def test_root():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/")
     assert response.status_code == 200
-    # assert response.json() == {"message": "Tomato"}
+
+
+def test_create_album(client: TestClient):
+    client = TestClient(app)  #
+    test_data = {"name": "Blood Sugar Sex Magik", "artist": "RHCP", "year": "1991"}
+    response = client.post("/api/v1/albums", json=test_data)
+    data = response.json()
+    assert response.status_code == 201
+
+    assert data["name"] == test_data.get("name")
+    assert data["artist"] == test_data.get("artist")
